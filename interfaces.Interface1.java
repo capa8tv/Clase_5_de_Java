a026a7f34bb701e117b5ad8266227b26adc9dff4
Capa8.tv <canalcapa8@gmail.com>
2015-09-16 22:40:54 -0400

    interfaces.Interface1.java

---
+++ interfaces.Interface1.java
@@ -0,0 +1,11 @@
+
+package interfaces;
+
+public interface Interface1 
+{
+    //declarando un método en una interface
+    public String getNombre();
+    public void setNombre(String nombre);
+    public String getCorreo();
+    public void setCorreo(String correo);
+}
